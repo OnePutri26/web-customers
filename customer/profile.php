c702ac70a88c7e44c1049740aaddf636f5b5a880
OnePutri26 <onep53475@gmail.com>
2026-09-04 14:13:06 +0700

    tes commit

--- customer/profile.php
+++ customer/profile.php
@@ -3,7 +3,7 @@ session_start();
 require_once '../config/database.php';
 
 // Cek login
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) { 
     header("Location: ../login.php");
     exit;
 }
